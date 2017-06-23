475c09c72527e9c8f33f14571b47aa3876d85ff2
tc9011 <tc9011@126.com>
2017-06-23 23:08:44 +0800

    delete unused import

--- App/src/app/app.routes.ts
+++ App/src/app/app.routes.ts
@@ -1,7 +1,3 @@
-import { RouterModule } from '@angular/router';
-import {HomepageComponent} from "./homepage/homepage.component";
-import { AppComponent } from './app.component';
-
 export const appRoutes=[
   {
     path:'',
