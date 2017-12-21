e27182d635da18272fe903d930784425b67b6a4c
tc9011 <tc9011@126.com>
2017-12-21 18:21:14 +0800

    feat(http): add options in http services

--- App/src/app/shared/services/http.service.ts
+++ App/src/app/shared/services/http.service.ts
@@ -8,19 +8,19 @@ export class HttpService {
 
   constructor(private http: HttpClient) { }
 
-  public getData(api:string):Observable<any>{
+  public getData(api:string, options = {}):Observable<any>{
     return this.http.get(api);
   }
 
-  public postData(api:string, data:any):Observable<any>{
+  public postData(api:string, data:any, options = {}):Observable<any>{
     return this.http.post(api, data);
   }
 
-  public deleteData(api:string):Observable<any>{
+  public deleteData(api:string, options = {}):Observable<any>{
     return this.http.delete(api);
   }
 
-  public putData(api:string, data:any):Observable<any>{
+  public putData(api:string, data:any, options = {}):Observable<any>{
     return this.http.put(api,  data);
   }
 }
