e009a5cd01fa7f8d82cb15290f6c208347c71340
tc9011 <tc9011@126.com>
2018-10-23 19:57:14 +0800

    refactor(shared): refactor shared module

--- App/src/app/shared/shared.module.ts
+++ App/src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import {
   AlertModule,
@@ -52,7 +52,7 @@ import { TranslateModule } from '@ngx-translate/core';
 const sharedModule = [
   CommonModule,
   FormsModule,
-  HttpClientModule,
+  RouterModule,
   ReactiveFormsModule,
   NgZorroAntdModule,
   AlertModule,
