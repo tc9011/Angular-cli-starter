0e0962e7ab76a83feeb7a2a6d32995edc6a59f32
tc9011 <tc9011@126.com>
2018-10-23 18:20:13 +0800

    build(build): add hmr

--- App/src/main.ts
+++ App/src/main.ts
@@ -3,12 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
 import { hmrBootstrap } from './hmr';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
 
@@ -20,5 +15,5 @@ if (environment.hmr) {
     console.log('Are you using the --hmr flag for ng serve?');
   }
 } else {
-  bootstrap().catch(err => console.log(err));
+  bootstrap();
 }
