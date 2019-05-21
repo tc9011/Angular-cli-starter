2ef006f3066b7cde5008e98a3d5ee74f852284fe
tc9011 <tc9011@126.com>
2019-05-22 00:39:37 +0800

    refactor(animations):change animation interval

--- App/src/app/shared/animations/fly-in.ts
+++ App/src/app/shared/animations/fly-in.ts
@@ -3,14 +3,14 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
 export const flyIn = trigger('flyIn', [
   state('in', style({transform: 'translateX(0)'})),
   transition('void => *', [
-    animate(300, keyframes([
+    animate(800, keyframes([
       style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
       style({opacity: 1, transform: 'translateX(25px)', offset: 0.3}),
       style({opacity: 1, transform: 'translateX(0)', offset: 1.0})
     ]))
   ]),
   transition('* => void', [
-    animate(300, keyframes([
+    animate(800, keyframes([
       style({opacity: 1, transform: 'translateX(0)', offset: 0}),
       style({opacity: 1, transform: 'translateX(-25px)', offset: 0.7}),
       style({opacity: 0, transform: 'translateX(100%)', offset: 1.0})
